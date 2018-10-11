1db813905478f15776e84a7ba75a383dbdbcda43
Kyle Mamiit (New) <KYLESMYLE@dhcp-v079-186.mobile.uci.edu>
2018-10-10 23:25:32 -0700

    Finished milestone 1

---
+++ Pods/Target Support Files/Pods-tumblrClone/Pods-tumblrClone-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tumblrClone : NSObject
+@end
+@implementation PodsDummy_Pods_tumblrClone
+@end
---
+++ Pods/Target Support Files/Pods-tumblrCloneTests/Pods-tumblrCloneTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tumblrCloneTests : NSObject
+@end
+@implementation PodsDummy_Pods_tumblrCloneTests
+@end
